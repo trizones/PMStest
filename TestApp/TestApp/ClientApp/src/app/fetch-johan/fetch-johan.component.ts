f7c8afe3c896d595ac26146c33cde6d1a5692285
johanfransson <johanfransson13@gmail.com>
2019-02-18 14:01:42 +0100

    Lagt till en ny COMPONENT , "TESTBOCK"
    här kommer jag göra mina försök på APIer

---
+++ TestApp/TestApp/ClientApp/src/app/fetch-johan/fetch-johan.component.ts
@@ -0,0 +1,9 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-fetch-johan',
+  templateUrl: './fetch-johan.component.html'
+})
+export class FetchJohanComponent {
+}
