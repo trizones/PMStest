85e98994e4a2739360f13533bb043d14b1213fb0
johanfransson <johanfransson13@gmail.com>
2019-02-20 11:37:27 +0100

    blabla

--- TestApp/TestApp/ClientApp/src/app/fetch-johan/fetch-johan.component.ts
+++ TestApp/TestApp/ClientApp/src/app/fetch-johan/fetch-johan.component.ts
@@ -7,8 +7,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchJohanComponent {
 
-  // METOD FÖR ATT KUNNA HÄMTA FRÅN API ????
+  public getTrello() {
 
+
+
+
+
+  }
+  // METOD FÃ–R ATT KUNNA HÃ„MTA FRÃ…N API ????
+
+//Xhttps://api.trello.com/1/members/me/boards?key={8647cda40947c5f59daaa1c3f5173a1a}&token={5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff}
 
   
 }
