10d9e5c62032306e9224e2bafaced57afaf666e1
johanfransson <johanfransson13@gmail.com>
2019-02-20 15:28:20 +0100

    heheh

--- TestApp/TestApp/ClientApp/src/app/fetch-johan/test.js
+++ TestApp/TestApp/ClientApp/src/app/fetch-johan/test.js
@@ -1,12 +1,23 @@
+var = trello;
 function trelloApi() {
 
-  loadJSON('https://api.trello.com/1/members/me/boards?key=8647cda40947c5f59daaa1c3f5173a1a&token=5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff')
+  loadJSON('https://api.trello.com/1/members/me/boards?key=8647cda40947c5f59daaa1c3f5173a1a&token=5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff', gotData)
+}
 
-
-
-  return 
+function gotData(data) {
+  trello = data;
 }
 
 
+
+function countDown() {
+  var nr = 0;
+
+  foreach(typeof projekt in trello){
+    nr++;
+    
+  }
+  document.getElementById("trelloInfo").innerHTML = nr;
+}
   //GET /1/members/me/boards
   https://api.trello.com/1/members/me/boards?key={8647cda40947c5f59daaa1c3f5173a1a}&token={5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff}
