af7e8f7ebaf1c08431854008ad49c5bf2e8d2af0
johanfransson <johanfransson13@gmail.com>
2019-02-25 15:45:26 +0100

    LÖST ALLT // Johan Christer Fransson

--- TestApp/TestApp/ClientApp/src/app/app.module.ts
+++ TestApp/TestApp/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -23,6 +24,7 @@ import { FetchJohanComponent } from './fetch-johan/fetch-johan.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    HttpModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
--- TestApp/TestApp/ClientApp/src/app/fetch-johan/fetch-johan.component.ts
+++ TestApp/TestApp/ClientApp/src/app/fetch-johan/fetch-johan.component.ts
@@ -1,34 +1,34 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Http, Response } from '@angular/http';
+import { Http, Response, HttpModule } from '@angular/http';
 import "rxjs/Rx";
-import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-fetch-johan',
   templateUrl: './fetch-johan.component.html'
 })
 export class FetchJohanComponent {
+
   public projektCount = 0;
-  data: Object;
   private baseUrl = "https://api.trello.com/1/members/me/boards?key=8647cda40947c5f59daaa1c3f5173a1a&token=5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff";  // API för att returnera en array av boards
-  constructor(private http: Http) { }
+  data: Object;
+
+    constructor(public http: Http) {
+    }
 
 
-  //public projektCounter() {
-
+  public projektCounter(){
     
-  //  var antalProjekt = 0;
+     
+    var antalProjekt = 0;
 
-  //  this.http.request(this.baseUrl)
-  //    .subscribe((res: Response) => {
-  //      this.data = res.json().result;
+    this.http.request(this.baseUrl)
+        .subscribe((res: Response) => {
+          this.data = res.json();
+         
 
-  //    });
+        });
 
-
-    
-
-  //  this.projektCount = antalProjekt;
-  //}
+    this.projektCount = antalProjekt;   //<------------ DET JAG VILL FÅ UT 
+  }
 }
