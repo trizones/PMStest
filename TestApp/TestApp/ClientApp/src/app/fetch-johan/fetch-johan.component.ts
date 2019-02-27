43dd588be93f122d98bb4f0e95fa6c8af522f7eb
johanfransson <johanfransson13@gmail.com>
2019-02-27 16:29:34 +0100

    FÖRSTA SPRINTEN klaro

--- TestApp/TestApp/ClientApp/src/app/fetch-johan/fetch-johan.component.ts
+++ TestApp/TestApp/ClientApp/src/app/fetch-johan/fetch-johan.component.ts
@@ -17,17 +17,16 @@ export class FetchJohanComponent {
     }
 
 
-  public projektCounter(){
-    
-     
-    var antalProjekt = 0;
+  public getProjects(){
 
     this.http.request(this.baseUrl)
         .subscribe((res: Response) => {
           this.data = res.json();
-         
+      });
+  }
 
-        });
+  public projectCounter() : void {
+    this.projektCount = Object.keys(this.data).length;
   }
 
   public postBoard() {
@@ -41,8 +40,14 @@ export class FetchJohanComponent {
       }
     });
 
-    xhr.open("POST", "https://api.trello.com/1/boards/?name=Newboard&defaultLabels=true&defaultLists=true&keepFromSource=none&prefs_permissionLevel=private&prefs_voting=disabled&prefs_comments=members&prefs_invitations=members&prefs_selfJoin=true&prefs_cardCovers=true&prefs_background=blue&prefs_cardAging=regular&key=8647cda40947c5f59daaa1c3f5173a1a&token=5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff");
 
+    var projectName = ((document.getElementById("text1") as HTMLInputElement).value);
+    var choosenType = ((document.getElementById("choosenType1") as HTMLInputElement).value);
+
+    // Skapar en ny board
+    //xhr.open("POST", "https://api.trello.com/1/boards/?name=" + projectName + "&defaultLabels=true&defaultLists=true&keepFromSource=none&prefs_permissionLevel=private&prefs_voting=disabled&prefs_comments=members&prefs_invitations=members&prefs_selfJoin=true&prefs_cardCovers=true&prefs_background=blue&prefs_cardAging=regular&key=8647cda40947c5f59daaa1c3f5173a1a&token=5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff");
+    // Ska skapa en ny kopia av Hasses board
+    xhr.open("POST", "https://api.trello.com/1/boards/?name=" + projectName + "&defaultLabels=true&defaultLists=true&idBoardSource=" + choosenType + "&keepFromSource=none&prefs_permissionLevel=private&prefs_voting=disabled&prefs_comments=members&prefs_invitations=members&prefs_selfJoin=true&prefs_cardCovers=true&prefs_background=blue&prefs_cardAging=regular&key=8647cda40947c5f59daaa1c3f5173a1a&token=5e73a3d20653d1e9f97812fa1572a61499b84ffd6954f1b33f4f93d69fd0fdff");
     xhr.send(data);
   }
 
